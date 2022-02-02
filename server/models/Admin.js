a082cb293097410d217a99465bed8abb28ce74ea
brkybzkrt <b9bozkurt959@gmail.com>
2022-02-02 22:15:36 +0300

    User roles added to models

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -6,7 +6,8 @@ const AdminModel= new Schema({
 
     username: {type:String,required:true},
     password:{type:String,required:true},
-    email:{type:String,required:true}
+    email:{type:String,required:true},
+    role:{type:String,default:"admin"}
 },{versionKey:false});
 
 
